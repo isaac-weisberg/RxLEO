8c513de87d930a29cdec989e6d34bc9143b79b20
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-11-10 14:46:08 +0700

    Migrate LEOArrayResponse

---
+++ RxLEO/LEOArrayResponse.swift
@@ -0,0 +1,11 @@
+//
+//  LEOArrayResponse.swift
+//  RxLEO
+//
+//  Created by Isaac Weisberg on 11/10/18.
+//  Copyright © 2018 Isaac Weisberg. All rights reserved.
+//
+
+open class LEOArrayResponse<Item: Decodable>: LEOObjectResponse<[Item]> {
+    
+}
