a7a1ce1555a701207e12e03cd0ddb1c25aeed97f
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-11-10 15:01:37 +0700

    Work a bit on the LEOListResponse's API portability

--- RxLEO/LEOListResponse.swift
+++ RxLEO/LEOListResponse.swift
@@ -8,10 +8,37 @@
 
 open class LEOListModel<Item: Decodable>: Decodable {
     public let items: [Item]
+    
+    // TODO: make clear whether if these are supposed to be optional or not
     public let nextCursor: String?
     public let prevCursor: String?
 }
 
 open class LEOListResponse<Item: Decodable>: LEOObjectResponse<LEOListModel<Item>> {
     
+    /**
+     The original implementation had an API that looked pretty much like this:
+     
+    public var data: [Item] {
+        return self.data.items
+    }
+     
+     Considering the fact that we are massively reusing the parsing behavior
+     from the `LEOObjectResponse`, the data property is already successfully
+     poiting to the `LEOListModel<Item>`, and thus is different.
+     I could of course go on to implement the custom parsing logic
+     so that the items of the `LEOListModel` become the actual value of `data`
+     and the API perfectly reproduces the original behavior, however uhm no.
+    */
+    
+    // TODO: document out this ^^^ behavior in the API migration guide.
+    
+    
+    public var nextCursor: String? {
+        return data.nextCursor
+    }
+    
+    public var prevCursor: String? {
+        return data.prevCursor
+    }
 }
