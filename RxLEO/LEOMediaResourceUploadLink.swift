3522e685da7de58261430bd2e3eee3a7dc979630
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-11-10 15:12:52 +0700

    Migrate LEOMediaResourceUploadLink

---
+++ RxLEO/LEOMediaResourceUploadLink.swift
@@ -0,0 +1,12 @@
+//
+//  LEOMediaResourceUploadLink.swift
+//  RxLEO
+//
+//  Created by Isaac Weisberg on 11/10/18.
+//  Copyright © 2018 Isaac Weisberg. All rights reserved.
+//
+
+open class LEOMediaResourceUploadLink: Decodable {
+    public let resourceId: String
+    public let uploadUrl: String
+}
