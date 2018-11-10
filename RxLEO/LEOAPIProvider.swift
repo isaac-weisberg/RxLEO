c68cda57417f5a98869e0e5f6ab6bfdd7fb6ba07
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-11-10 21:15:58 +0700

    Add the LEOAPIProvider concept and the default implemetation which will be provided by the framework

---
+++ RxLEO/LEOAPIProvider.swift
@@ -0,0 +1,11 @@
+//
+//  LEOAPIProvider.swift
+//  RxLEO
+//
+//  Created by Isaac Weisberg on 11/10/18.
+//  Copyright © 2018 Isaac Weisberg. All rights reserved.
+//
+
+public protocol LEOAPIProvider {
+    
+}
---
+++ RxLEO/RxLEOAPIProvider.swift
@@ -0,0 +1,11 @@
+//
+//  RxLEOAPIProvider.swift
+//  RxLEO
+//
+//  Created by Isaac Weisberg on 11/10/18.
+//  Copyright © 2018 Isaac Weisberg. All rights reserved.
+//
+
+final public class RxLEOAPIProvider: LEOAPIProvider {
+    
+}
