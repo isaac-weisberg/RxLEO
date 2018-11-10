b12570f18cb7230e34b3b0fb6218e17ad7f19a82
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-11-10 21:18:46 +0700

    Add a concept of LEORoute

---
+++ RxLEO/LEORoute.swift
@@ -0,0 +1,11 @@
+//
+//  LEORoute.swift
+//  RxLEO
+//
+//  Created by Isaac Weisberg on 11/10/18.
+//  Copyright © 2018 Isaac Weisberg. All rights reserved.
+//
+
+public protocol LEORoute {
+    
+}
