b8a10cf7c80dec3631f72219c2e5095b2e1537c9
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-11-11 12:46:27 +0700

    Add path and url assembly getter to the LEOCommonRoute

--- RxLEO/LEOCommonRoute.swift
+++ RxLEO/LEOCommonRoute.swift
@@ -9,5 +9,34 @@
 import Foundation
 
 public protocol LEOCommonRoute {
+    /**
+     This is the base endpoint of the server.
+     The proper usage is to define in a project
+     dependent on this framework a default extension
+     which will return the endpoint pointing to the
+     root of the Leopold API.
+     
+     For instance, https://meproject.com/api/v1/
+     */
     var endpoint: URL { get }
+    
+    /**
+     This string specifies a path relative
+     to the endpoint inside the API.
+     
+     Commonly, it's just appended to the endpoint
+     */
+    var path: String { get }
+    
+    /**
+     This getter will be used to get the actual URL,
+     with endpoint and path being taken into account.
+     */
+    var assembledUrl: URL { get }
+}
+
+public extension LEOCommonRoute {
+    var assembledUrl: URL {
+        return endpoint.appendingPathComponent(path)
+    }
 }
